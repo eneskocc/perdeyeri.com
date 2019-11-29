d0a92fd61aa3891c66422ff66e0f082c86ba2e9c
Enes koç <32553624+eneskocc@users.noreply.github.com>
2019-11-29 15:06:15 +0300

    create

---
+++ index.js
@@ -0,0 +1,35 @@
+const perdeTur = document.getElementsByClassName("perde-tur-hover");
+
+
+
+var swiper = new Swiper('.swiper-container1', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    loop: true,
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    loop: true,
+    loopedSlides: 5, //looped slides should be the same
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: galleryThumbs,
+    },
+});
